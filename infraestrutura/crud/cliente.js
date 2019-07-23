91de5c32af3cbc4d48a4917e7cf42e8c375b97e5
Alexandre Teixeira <alexanddy99@gmail.com>
2019-07-22 21:33:58 -0300

    Commit inicial

---
+++ infraestrutura/crud/cliente.js
@@ -0,0 +1,43 @@
+const executaQuery = require('../database/queries')
+
+class Cliente {
+  lista() {
+    const sql = 'SELECT * FROM Clientes'
+
+    return executaQuery(sql)
+  }
+
+  buscaPorId(id) {
+    const sql = `SELECT * FROM Clientes WHERE id=${id}`
+
+    executaQuery(sql)
+  }
+
+  adiciona(item) {
+    const { nome, cpf } = item
+    const sql = `INSERT INTO Clientes(nome, CPF) VALUES('${nome}', '${cpf}')`
+
+    return executaQuery(sql).then(resposta =>
+      ({
+        id: resposta.insertId,
+        nome,
+        cpf
+      })
+    )
+  }
+
+  atualiza(novoItem, id) {
+    const { nome, cpf } = novoItem
+    const sql = `UPDATE Clientes SET nome='${nome}', CPF='${cpf}' WHERE id=${id}`
+
+    executaQuery(sql)
+  }
+
+  deleta(id) {
+    const sql = `DELETE FROM Clientes WHERE id=${id}`
+
+    executaQuery(sql)
+  }
+}
+
+module.exports = new Cliente

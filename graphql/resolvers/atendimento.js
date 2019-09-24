0f5f0ff7ef2777726dcc353e0e552021f8a609c6
alexandre99 <alexanddy99@gmail.com>
2019-09-24 09:49:43 -0300

    ajustando estrutura dos arquivos

---
+++ graphql/resolvers/atendimento.js
@@ -0,0 +1,16 @@
+const Operacoes = require('../../infraestrutura/operations')
+const Atendimentos = new Operacoes('atendimento')
+
+const resolvers = {
+    Query: {
+        atendimento: (root, { id }) => Atendimentos.buscaPorId(id),
+        atendimentos: () => Atendimentos.lista()
+    },
+    Mutation: {
+        adicionarAtendimento: (root, params) => Atendimentos.adiciona(params),
+        atualizarAtendimento: (root, params) => Atendimentos.atualiza(params),
+        deletarAtendimento: (root, { id }) => Atendimentos.deleta(id)
+    }
+}
+
+module.exports = resolvers;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const { GraphQLServer } = require('graphql-yoga')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
-const Operacoes = require('./infraestrutura/operations')
+const resolvers = require('./graphql/resolvers/index')
+const typeDefs = require('./graphql/schemas/index')
 
 conexao.connect(erro => {
   if (erro) {
@@ -13,48 +14,9 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
-const Clientes = new Operacoes('cliente')
-const Pets = new Operacoes('pet')
-const Servicos = new Operacoes('servico')
-const Atendimentos = new Operacoes('atendimento')
-
-const resolvers = {
-  Query: {
-    status: () => 'Servidor rodando',
-    clientes: () => Clientes.lista(),
-    cliente: (root, { id }) => Clientes.buscaPorId(id),
-
-    pets: () => Pets.lista(),
-    pet: (root, { id }) => Pets.buscaPorId(id),
-
-    servicos: () => Servicos.lista(),
-    servico: (root, { id }) => Servicos.buscaPorId(id),
-
-    atendimento: (root, { id }) => Atendimentos.buscaPorId(id),
-    atendimentos: () => Atendimentos.lista()
-  },
-  Mutation: {
-    adicionarCliente: (root, params) => Clientes.adiciona(params),
-    atualizarCliente: (root, params) => Clientes.atualiza(params),
-    deletarCliente: (root, { id }) => Clientes.deleta(id),
-
-    adicionarPet: (root, params) => Pets.adiciona(params),
-    atualizarPet: (root, params) => Pets.atualiza(params),
-    deletarPet: (root, { id }) => Pets.deleta(id),
-
-    adicionarServico: (root, params) => Servicos.adiciona(params),
-    atualizarServico: (root, params) => Servicos.atualiza(params),
-    deletarServico: (root, { id }) => Servicos.deleta(id),
-
-    adicionarAtendimento: (root, params) => Atendimentos.adiciona(params),
-    atualizarAtendimento: (root, params) => Atendimentos.atualiza(params),
-    deletarAtendimento: (root, { id }) => Atendimentos.deleta(id)
-  }
-}
-
 const servidor = new GraphQLServer({
   resolvers,
-  typeDefs: './schema.graphql'
+  typeDefs: typeDefs
 })
 
 servidor.start(() => console.log("Servidor rodando"))
